595935a2a67f8d5960d12126ae2c80a340caf2a1
David <davidsokol112@gmail.com>
2023-07-23 22:25:29 +0300

    creating useLocalStorageSec

--- src/useLocalStorage.ts
+++ src/useLocalStorage.ts
@@ -20,3 +20,24 @@ export function useLocalStorage<T>(key: string, initalValue: T | (() => T)) {
 
 	return [value, setValue] as [T, typeof setValue]
 }
+
+export function useLocalStorageSec<T>(key: string, initalValue: T | (() => T)) {
+	const [value, setValue] = useState<T>(() => {
+		const jsonValue = localStorage.getItem(key)
+		if(jsonValue == null) {
+			if(typeof initalValue === 'function') {
+				return (initalValue as () => T)()
+			} else {
+				return initalValue
+			}
+		} else {
+			return JSON.parse(jsonValue)
+		}
+	})
+
+	useEffect(() => {
+		localStorage.setItem(key, JSON.stringify(value))
+	}, [value, key])
+
+	return [value, setValue] as [T, typeof setValue]
+}
