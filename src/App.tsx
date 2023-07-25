12d088a0185eaff42ac18704b0e56e4ee8121083
David <davidsokol112@gmail.com>
2023-07-25 21:38:36 +0300

    create navigation on note page

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { useLocalStorage } from './useLocalStorage'
 import { useMemo } from 'react'
 import { v4 as uuidV4 } from 'uuid'
 import { NoteList } from './NoteList/NoteList'
+import { NoteLayout } from './NoteLayout/NoteLayout'
+import { Note } from './Note/Note'
 
 export type Note = {
     id: string
@@ -52,23 +54,37 @@ function App() {
                 {
                     ...data,
                     id: uuidV4(),
-                    tagIds: tags.map(tag => tag.id),
+                    tagIds: tags.map((tag) => tag.id),
                 },
             ]
         })
     }
 
-	function addTag(tag: Tag) {
-		setTags(prev => [...prev, tag])
-	}
+    function addTag(tag: Tag) {
+        setTags((prev) => [...prev, tag])
+    }
 
     return (
         <Container className="my-4">
             <Routes>
-                <Route path="/" element={<NoteList notes={notesWithTags} availableTags={tags}/>} />
-                <Route path="/new" element={<NewNote onSubmit={onCreateNote} onAddTag={addTag} availableTags={tags}/>} />
-                <Route path="/:id">
-                    <Route index element={<h1>Show</h1>} />
+                <Route
+                    path="/"
+                    element={
+                        <NoteList notes={notesWithTags} availableTags={tags} />
+                    }
+                />
+                <Route
+                    path="/new"
+                    element={
+                        <NewNote
+                            onSubmit={onCreateNote}
+                            onAddTag={addTag}
+                            availableTags={tags}
+                        />
+                    }
+                />
+                <Route path="/:id" element={<NoteLayout notes={notesWithTags}/>}>
+                    <Route index element={<Note />} />
                     <Route path="edit" element={<h1>Edit</h1>} />
                 </Route>
                 <Route path="*" element={<Navigate to="/" />} />
